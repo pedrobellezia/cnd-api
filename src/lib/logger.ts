9b9f27be299e129e686f3c466a32497d1e7a894a
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-07-02 15:13:15 -0300

    feat(logger): adiciona formatacao amigavel com pino-pretty

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,5 +1,19 @@
 import pino from "pino";
 
 export const logger = pino({
-  level: process.env.LOG_LEVEL || "info"
+  level: process.env.LOG_LEVEL || "info",
+  formatters: {
+    level: (label) => {
+      return { level: label.toUpperCase() };
+    },
+  },
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
+      ignore: "pid,hostname",
+      singleLine: true,
+    },
+  },
 });
