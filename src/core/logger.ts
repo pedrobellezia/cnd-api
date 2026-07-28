e5824520671f34365362743698db8567c51deb4f
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-07-28 15:07:32 -0300

    refactor(core): desabilita formatacao pino-pretty em producao

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,13 +1,18 @@
 import pino from "pino";
 
+// Usa pino-pretty apenas se não estiver em ambiente de produção
+const isDev = process.env.NODE_ENV !== "production";
+
 export const logger = pino({
   level: process.env.LOG_LEVEL || "info",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: "SYS:standard",
-      ignore: "pid,hostname",
-    },
-  },
+  transport: isDev
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
+          ignore: "pid,hostname",
+        },
+      }
+    : undefined,
 });
